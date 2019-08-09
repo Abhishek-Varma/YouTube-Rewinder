58e37ca2a960b0aeb5860cb4307f1e8bd9497199
Abhishek-Varma <avarma094@gmail.com>
2019-08-09 12:07:52 +0530

    Fetch link of the song
    Successfully fetches the link to the song.

--- YouTube_Rewinder.py
+++ YouTube_Rewinder.py
@@ -1,9 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chromeDriver = webdriver.Chrome()
 chromeDriver.get("https://www.youtube.com")
 search_field = chromeDriver.find_element_by_xpath("//input[@id='search']")
 search_field.clear() # clears any character (if any) present in input field
-search_field.send_keys("Abhishek Varma Bekhayali")
+song_to_search = "Abhishek Varma Bekhayali"
+search_field.send_keys(song_to_search)
 search_field.send_keys(Keys.RETURN)
+
+wait = WebDriverWait(chromeDriver, 10)
+try:
+	wait.until(EC.title_contains(song_to_search))
+	song_link = chromeDriver.find_element_by_xpath("//ytd-video-renderer[1]/div/ytd-thumbnail/a[1]")
+	print(song_link.get_attribute('href'))
+except:
+	print("It took longer than 10 seconds. Check your net connectivity!")
+	chromeDriver.quit()
