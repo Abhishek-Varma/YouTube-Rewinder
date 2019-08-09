9f4ef8ca8b97bf48af4622f7b3083bcb81115d63
Abhishek-Varma <avarma094@gmail.com>
2019-08-09 11:36:12 +0530

    Input field successfully recognized
    Input field is being recognized successfully by XPATH
    //input[@id='search']
    The search keywords are successfully entered and fired.

--- YouTube_Rewinder.py
+++ YouTube_Rewinder.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 
 chromeDriver = webdriver.Chrome()
 chromeDriver.get("https://www.youtube.com")
-search_field = chromeDriver.find_element_by_id("search")
+search_field = chromeDriver.find_element_by_xpath("//input[@id='search']")
 search_field.clear() # clears any character (if any) present in input field
 search_field.send_keys("Abhishek Varma Bekhayali")
 search_field.send_keys(Keys.RETURN)
