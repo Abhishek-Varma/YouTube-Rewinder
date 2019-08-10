8f2a58e931fe7c8ce368352e1a8197258bbbe4ab
Abhishek-Varma <avarma094@gmail.com>
2019-08-10 16:09:47 +0530

    Added menu
    At the beginning of the program, following things are being taken as input :-
    1. Total no. of songs/videos
    2. Search phrase for each song/video
    3. Total repeatation needed for each song/video
    
    After this we call the function 'YouTube_Rewind'

--- YouTube_Rewinder.py
+++ YouTube_Rewinder.py
@@ -2,65 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
+def YouTube_Rewind():
+	chromeDriver = webdriver.Chrome()
+	chromeDriver.get("https://www.youtube.com")
+	for song_to_search in song_list:
+		search_field = chromeDriver.find_element_by_xpath("//input[@id='search']")
+		search_field.clear() # clears any character (if any) present in input field
+		# song_to_search = "Abhishek Varma Bekhayali"
+		search_field.send_keys(song_to_search)
+		search_field.send_keys(Keys.RETURN)
 
-# class element_has_class(object):
-# 	def __init__(self, locator, css_class):
-# 	self.locator = locator
-# 	self.css_class = css_class
+		wait = WebDriverWait(chromeDriver, 10)
+		try:
+			wait.until(EC.title_contains(song_to_search))
+		except:
+			print("It took longer than 10 seconds. Check your net connectivity!")
+			chromeDriver.quit()
 
-# 	def __call__(self, driver):
-# 	element = driver.find_element()   # Finding the referenced element
-# 	if self.css_class in element.get_attribute("class"):
-#     	return element
-# 	else:
-#     	return False
+		current_url = chromeDriver.current_url
+		song = chromeDriver.find_element_by_xpath("//ytd-video-renderer[1]/div/ytd-thumbnail/a[1]")
+		song_link = song.get_attribute('href')
+		song.click()
 
+		try:
+			# need to wait until url changes. Only after that should I wait for the 'video' to be present!
+			wait.until(EC.url_changes(current_url))
+		except:
+			print("It took longer than 10 seconds for the url to change. Check your net connectivity!")
+			chromeDriver.quit()
 
-chromeDriver = webdriver.Chrome()
-chromeDriver.get("https://www.youtube.com")
-search_field = chromeDriver.find_element_by_xpath("//input[@id='search']")
-search_field.clear() # clears any character (if any) present in input field
-song_to_search = "Abhishek Varma Bekhayali"
-search_field.send_keys(song_to_search)
-search_field.send_keys(Keys.RETURN)
-
-wait = WebDriverWait(chromeDriver, 10)
-try:
-	wait.until(EC.title_contains(song_to_search))
-except:
-	print("It took longer than 10 seconds. Check your net connectivity!")
+		rewind = replay_count
+		while rewind > 0:
+			try:
+				play_button = chromeDriver.find_element_by_xpath("//button[@class='ytp-play-button ytp-button']")
+			except:
+				continue
+			if play_button.get_attribute('title')=="Replay":
+				play_button.click()
+				rewind = rewind - 1
 	chromeDriver.quit()
 
-current_url = chromeDriver.current_url
-print(current_url)
-song = chromeDriver.find_element_by_xpath("//ytd-video-renderer[1]/div/ytd-thumbnail/a[1]")
-song_link = song.get_attribute('href')
-song.click()
-print(song_link)
 
-try:
-	# need to wait until url changes. Only after that should I wait for the 'video' to be present!
-	wait.until(EC.url_changes(current_url))
-	# wait.until(EC.visibilityOfElementLocated(By.CLASS("ytp-play-button ytp-button")))
-	# print("Current url -> "+str(chromeDriver.current_url))
-except:
-	print("It took longer than 10 seconds for the url to change. Check your net connectivity!")
-	chromeDriver.quit()
-
-while True:
-	try:
-		play_button = chromeDriver.find_element_by_xpath("//button[@class='ytp-play-button ytp-button']")
-	except:
-		continue
-	if play_button.get_attribute('title')=="Replay":
-		play_button.click()
-		break
-
-# total_duration = chromeDriver.find_element_by_xpath("//span[@class='ytp-time-duration']")
-# print(total_duration.getText())
-# time.sleep(2)
-# play_button.click()
-# time_duration = chromeDriver.find_element_by_xpath("//span[@class='ytp-time-duration']").getText()
-# print(time_duration)
+total_songs = int(input("How many songs? : "))
+song_list = []
+for i in range(total_songs):
+	search_phrase = input("Enter the song no."+ str(i+1)+" : ")
+	song_list.append(search_phrase)
+replay_count = int(input("How long do you want to replay each song? : "))
+YouTube_Rewind()
