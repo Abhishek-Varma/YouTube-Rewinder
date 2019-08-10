545563440ace45cf07a93e97474655aa53c1c9de
Abhishek-Varma <avarma094@gmail.com>
2019-08-10 15:37:57 +0530

    Replay logic
    The replay logic works!
    I'm waiting for the pause/play button to change to replay button.
    Once it changes to replay button, I click on it and voila!

--- YouTube_Rewinder.py
+++ YouTube_Rewinder.py
@@ -2,6 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+# class element_has_class(object):
+# 	def __init__(self, locator, css_class):
+# 	self.locator = locator
+# 	self.css_class = css_class
+
+# 	def __call__(self, driver):
+# 	element = driver.find_element()   # Finding the referenced element
+# 	if self.css_class in element.get_attribute("class"):
+#     	return element
+# 	else:
+#     	return False
+
 
 chromeDriver = webdriver.Chrome()
 chromeDriver.get("https://www.youtube.com")
@@ -18,15 +33,34 @@ except:
 	print("It took longer than 10 seconds. Check your net connectivity!")
 	chromeDriver.quit()
 
+current_url = chromeDriver.current_url
+print(current_url)
 song = chromeDriver.find_element_by_xpath("//ytd-video-renderer[1]/div/ytd-thumbnail/a[1]")
+song_link = song.get_attribute('href')
 song.click()
+print(song_link)
 
-# try:
-# 	# need to wait until url changes. Only after that should I wait for the 'video' to be present!
-# 	video = wait.until(EC.presence_of_element_located("//video[1]"))
-# except:
-# 	print("2nd wala. It took longer than 10 seconds. Check your net connectivity!")
-# 	chromeDriver.quit()
+try:
+	# need to wait until url changes. Only after that should I wait for the 'video' to be present!
+	wait.until(EC.url_changes(current_url))
+	# wait.until(EC.visibilityOfElementLocated(By.CLASS("ytp-play-button ytp-button")))
+	# print("Current url -> "+str(chromeDriver.current_url))
+except:
+	print("It took longer than 10 seconds for the url to change. Check your net connectivity!")
+	chromeDriver.quit()
 
+while True:
+	try:
+		play_button = chromeDriver.find_element_by_xpath("//button[@class='ytp-play-button ytp-button']")
+	except:
+		continue
+	if play_button.get_attribute('title')=="Replay":
+		play_button.click()
+		break
+
+# total_duration = chromeDriver.find_element_by_xpath("//span[@class='ytp-time-duration']")
+# print(total_duration.getText())
+# time.sleep(2)
+# play_button.click()
 # time_duration = chromeDriver.find_element_by_xpath("//span[@class='ytp-time-duration']").getText()
 # print(time_duration)
